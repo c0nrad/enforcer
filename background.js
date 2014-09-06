86da8ecdb0d10bbb7f66408afe3a5952100246d8
Stuart Larsen <sclarsen@mtu.edu>
2014-09-05 23:02:41 -0400

    Can change policy and enabled/disable

--- background.js
+++ background.js
@@ -1,42 +1,43 @@
 'use strict';
 
-// Array to hold callback functions
-var callbacks = [];
-
-// // This function is called onload in the popup code
-// function getPageInfo(callback) {
-//     // Add the callback to the queue
-//     callbacks.push(callback);
-//     // Inject the content script into the current page
-//     chrome.tabs.executeScript(null, { file: 'content_script.js' });
-// };
-//
-// // Perform the callback when a request is received from the content script
-// chrome.extension.onMessage.addListener(function(request)  {
-//     // Get the first callback in the callbacks array
-//     // and remove it from the array
-//     console.log(request);
-//     var callback = callbacks.shift();
-//     // Call the callback function
-//     callback(request);
-// });
-
-console.log('I AM BACKGROUND');
-
-var POLICY = "default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self'; font-src 'self'; report-uri http://localhost:3000/endpoint/ad82cb7bb6fbdc4fa58a78b8e4efdfb4348ad2d1d7305015b3f4901b9d8b5836"
+var chrome = chrome || {};
+var policy = "default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self'; font-src 'self'; report-uri http://localhost:3000/endpoint/ad82cb7bb6fbdc4fa58a78b8e4efdfb4348ad2d1d7305015b3f4901b9d8b5836"
+var enabled = true;
 
 chrome.webRequest.onHeadersReceived.addListener(
   function(details) {
-    var out = []
-    out.push({name: 'Content-Security-Policy', value: POLICY});
+    var out = [];
+    console.log('using policy', policy);
+
+    if (enabled) {
+      out.push({name: 'Content-Security-Policy', value: policy});
+    }
 
     for (var i = 0; i < details.responseHeaders.length; ++i) {
-      if (details.responseHeaders[i].name === 'Server') {
-        console.log("SCORE");
+      if (details.responseHeaders[i].name === 'Content-Security-Policy') {
+        console.log('Previous Policy Found');
       }
       out.push(details.responseHeaders[i]);
 
     }
-    console.log(JSON.stringify(out));
     return { responseHeaders: out };
-  }, { urls: [ "<all_urls>"]}, [ "blocking", "responseHeaders"]);
+  }, { urls: [ '<all_urls>']}, [ 'blocking', 'responseHeaders']);
+
+
+chrome.extension.onMessage.addListener( function(request,sender,sendResponse) {
+    console.log('Request greeting', request.greeting);
+    if( request.greeting === 'getPolicy' ) {
+      sendResponse( {policy:policy} );
+    }
+    if (request.greeting === 'getEnabled' ) {
+      sendResponse( {enabled: enabled } );
+    }
+    if (request.greeting === 'setPolicy' ) {
+      console.log('setting policy to', request.policy);
+      policy = request.policy;
+    }
+    if (request.greeting === 'setEnabled') {
+      console.log('enabled', request.enabled);
+      enabled = request.enabled;
+    }
+});
--- popup.js
+++ popup.js
@@ -1,70 +1,147 @@
-// This callback function is called when the content script has been 
-// injected and returned its results
-function onPageInfo(o)  {
-    document.getElementById('title').value = o.title;
-    document.getElementById('url').value = o.url;
-    document.getElementById('summary').innerText = o.summary;
-}
+'use strict';
+var chrome = chrome || {};
 
-// Global reference to the status display SPAN
-var statusDisplay = null;
 
-// POST the data to the server using XMLHttpRequest
-function addBookmark() {
-    // Cancel the form submit
-    event.preventDefault();
 
-    // The URL to POST our data to
-    var postUrl = 'http://post-test.local.com';
+var app = angular.module('app', []);
 
-    // Set up an asynchronous AJAX POST request
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', postUrl, true);
+app.controller('MainController', function($scope, $rootScope, Policy) {
 
-    // Prepare the data to be POSTed
-    var title = encodeURIComponent(document.getElementById('title').value);
-    var url = encodeURIComponent(document.getElementById('url').value);
-    var summary = encodeURIComponent(document.getElementById('summary').value);
-    var tags = encodeURIComponent(document.getElementById('tags').value);
+  Policy.getPolicy();
+  Policy.getEnabled();
 
-    var params = 'title=' + title +
-                 '&url=' + url +
-                 '&summary=' + summary +
-                 '&tags=' + tags;
+  $scope.setPolicy = function() {
+    console.log($scope.policy);
+    Policy.setPolicy($scope.policy);
+  };
 
-    // Replace any instances of the URLEncoded space char with +
-    params = params.replace(/%20/g, '+');
+  $scope.setEnabled = function() {
+    console.log('set enabled', $scope.enabled);
+    Policy.setEnabled($scope.enabled);
+  };
 
-    // Set correct header for form data
-    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-
-    // Handle request state change events
-    xhr.onreadystatechange = function() {
-        // If the request completed
-        if (xhr.readyState == 4) {
-            statusDisplay.innerHTML = '';
-            if (xhr.status == 200) {
-                // If it was a success, close the popup after a short delay
-                statusDisplay.innerHTML = 'Saved!';
-                window.setTimeout(window.close, 1000);
-            } else {// Show what went wrong
-                statusDisplay.innerHTML = 'Error saving: ' + xhr.statusText;
-            }
-        }
-    };
-
-    // Send the request and set status
-    xhr.send(params);
-    statusDisplay.innerHTML = 'Saving...';
-}
-
-// When the popup HTML has loaded
-window.addEventListener('load', function(evt) {
-    // Handle the bookmark form submit event with our addBookmark function
-    document.getElementById('addbookmark').addEventListener('submit', addBookmark);
-    // Cache a reference to the status display SPAN
-    statusDisplay = document.getElementById('status-display');
-    // Call the getPageInfo function in the background page, injecting content_script.js
-    // into the current HTML page and passing in our onPageInfo function as the callback
-    chrome.extension.getBackgroundPage().getPageInfo(onPageInfo);
 });
+
+app.service('Policy', function($rootScope) {
+  var out = {};
+  out.getPolicy = function() {
+    chrome.extension.sendMessage({greeting: 'getPolicy'},
+      function (response) {
+          var policy = response.policy;
+          $rootScope.$apply(function() {
+            $rootScope.policy = policy;
+          });
+      });
+  };
+
+  out.getEnabled = function() {
+    chrome.extension.sendMessage({greeting: 'getEnabled'},
+      function (response) {
+          var enabled = response.enabled;
+          $rootScope.$apply(function() {
+            $rootScope.enabled = enabled;
+          });
+      });
+  };
+
+  out.setPolicy = function(policy) {
+    console.log('Setting policy!', policy);
+    chrome.extension.sendMessage({greeting: 'setPolicy', policy: policy});
+  };
+
+  out.setEnabled = function(enabled) {
+    chrome.extension.sendMessage({greeting: 'setEnabled', enabled: enabled});
+  };
+
+  return out;
+});
+
+
+
+// // This callback function is called when the content script has been
+// // injected and returned its results
+// function onPageInfo(o)  {
+//     document.getElementById('title').value = o.title;
+//     document.getElementById('url').value = o.url;
+//     document.getElementById('summary').innerText = o.summary;
+// }
+//
+// // Global reference to the status display SPAN
+// var statusDisplay = null;
+//
+// // POST the data to the server using XMLHttpRequest
+// function addBookmark() {
+//     // Cancel the form submit
+//     event.preventDefault();
+//
+//     // The URL to POST our data to
+//     var postUrl = 'http://post-test.local.com';
+//
+//     // Set up an asynchronous AJAX POST request
+//     var xhr = new XMLHttpRequest();
+//     xhr.open('POST', postUrl, true);
+//
+//     // Prepare the data to be POSTed
+//     var title = encodeURIComponent(document.getElementById('title').value);
+//     var url = encodeURIComponent(document.getElementById('url').value);
+//     var summary = encodeURIComponent(document.getElementById('summary').value);
+//     var tags = encodeURIComponent(document.getElementById('tags').value);
+//
+//     var params = 'title=' + title +
+//                  '&url=' + url +
+//                  '&summary=' + summary +
+//                  '&tags=' + tags;
+//
+//     // Replace any instances of the URLEncoded space char with +
+//     params = params.replace(/%20/g, '+');
+//
+//     // Set correct header for form data
+//     xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+//
+//     // Handle request state change events
+//     xhr.onreadystatechange = function() {
+//         // If the request completed
+//         if (xhr.readyState == 4) {
+//             statusDisplay.innerHTML = '';
+//             if (xhr.status == 200) {
+//                 // If it was a success, close the popup after a short delay
+//                 statusDisplay.innerHTML = 'Saved!';
+//                 window.setTimeout(window.close, 1000);
+//             } else {// Show what went wrong
+//                 statusDisplay.innerHTML = 'Error saving: ' + xhr.statusText;
+//             }
+//         }
+//     };
+//
+//     // Send the request and set status
+//     xhr.send(params);
+//     statusDisplay.innerHTML = 'Saving...';
+// }
+
+// function getPolicy() {
+//   chrome.extension.sendMessage({greeting: 'getPolicy'},
+//       function (response) {
+//           var policy = response.policy;
+//           document.getElementById('policy').innerText = policy;
+//       });
+// }
+
+// function setPolicy() {
+//   var policy = document.getElementById('policy').innerText;
+//   console.log('Sending policy', policy);
+//   chrome.extension.sendMessage({policy: policy});
+// }
+//
+// getPolicy();
+
+
+// // When the popup HTML has loaded
+// window.addEventListener('load', function(evt) {
+//     // Handle the bookmark form submit event with our addBookmark function
+//     document.getElementById('addbookmark').addEventListener('submit', addBookmark);
+//     // Cache a reference to the status display SPAN
+//     statusDisplay = document.getElementById('status-display');
+//     // Call the getPageInfo function in the background page, injecting content_script.js
+//     // into the current HTML page and passing in our onPageInfo function as the callback
+//     chrome.extension.getBackgroundPage().getPageInfo(onPageInfo);
+// });
